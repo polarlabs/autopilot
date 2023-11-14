c2344e75c22b921b9f7b941266c5a28b10e522e5
tokcum <tobias.mucke@gmail.com>
2023-11-14 23:17:04 +0100

    mod: refine post-install and enroll.

--- post-install.sh
+++ post-install.sh
@@ -6,8 +6,10 @@ REPO_URL="https://github.com/polarlabs/autopilot"
 
 apt --version >/dev/null 2>&1
 if [ $? -eq 0 ]; then
-  sudo apt install $ANSIBLE_PACKAGE
+  sudo apt-get -qq update
+  sudo apt-get --assume-yes --quiet install $ANSIBLE_PACKAGE
   sudo ansible-pull --url $REPO_URL $ANSIBLE_PLAYBOOK
 fi
 
 exit $?
+
